9cc2cb5aa150122c00f4538fcfa03c756458d8aa
MarcosDMT <marcosgav80@gmail.com>
2023-07-05 22:22:06 +0300

    added hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import img1 from '../assets/images/b1.png'
+import React from "react";
+import img1 from "../assets/images/b1.png";
+import img2 from "../assets/images/b2.png";
+import img3 from "../assets/images/b3.png";
+import img4 from "../assets/images/b18.png";
 
 const Hero = () => {
   return (
     <>
-    <section className='bg-primary h-[50vh] heroSection'>
-        <div>
-            <img className='h-72' src={img1} alt="" />
-        </div>
-        <div>
-            <img className='h-72' src={img1} alt="" />
-        </div>
-        <div>
-            <img className='h-72' src={img1} alt="" />
-        </div>
-    </section>
+      <section className="bg-primary h-[50vh] heroSection p-4 cursor-pointer">
+        <img
+          className="h-56 w-[40vw] object-cover rounded-xl"
+          src={img1}
+          alt=""
+        />
+        <img
+          className="h-56 w-[40vw] object-cover rounded-xl"
+          src={img2}
+          alt=""
+        />
+        <img
+          className="h-56 w-[40vw] object-cover rounded-xl"
+          src={img3}
+          alt=""
+        />
+        <img
+          className="h-56 w-[40vw] object-cover rounded-xl"
+          src={img4}
+          alt=""
+        />
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
